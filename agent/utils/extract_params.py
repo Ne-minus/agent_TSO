0f3433ac64a8bd9a229af126e34e2deee86ddae9
AlexanderPAI <petrov@backendpages.ru>
2025-10-30 18:58:18 +0300

    Tokens to logs

--- agent/utils/extract_params.py
+++ agent/utils/extract_params.py
@@ -259,8 +259,8 @@ class ParamExtractor:
         llm = self.get_llm(self.state)
         try:
             resp = await (prompt | llm).ainvoke({})
-            logger.debug(f"Вопрос: {question}")
-            logger.debug(f"Проверка истории диалога (есть ли ответ на вопрос): '{question}': {resp.content if hasattr(resp, 'content') else resp}")
+            logger.debug("Проверяем историю диалога")
+            logger.debug(f"'{question}': {resp.content if hasattr(resp, 'content') else resp}")
         except Exception as e:
             import traceback
             traceback.print_exc()
