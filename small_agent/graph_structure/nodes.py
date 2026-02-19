9a22b0667f8c6497a7a86e81680db5c4eea25e83
ne-minus <neminova2.0@gmail.com>
2026-02-19 13:34:12 +0300

    missing address handled

--- small_agent/graph_structure/nodes.py
+++ small_agent/graph_structure/nodes.py
@@ -12,15 +12,19 @@ from small_agent.prompts.prompt import create_prompt
 async def analyze_query(state: State, config: RunnableConfig, model) -> State:
     messages = list(state["messages"])
     system = SystemMessage(create_prompt())
+    address = state["address"].full_address
+    status = state["status"]
+
+    if not address:
+        print("no address")
+        messages.append("Важно: У пользователя не задан адрес в профиле Сберчат.")
+        status = "DONE"
 
     llm = model.bind_tools([user_interaction_tool, format_output_tool])
 
     response = await llm.ainvoke([system] + messages, config)
 
-    return {
-        "messages": [response],
-        "agent_message": response.content,
-    }
+    return {"messages": [response], "agent_message": response.content, "status": status}
 
 
 def should_route_scenario_reflect(state: State):
